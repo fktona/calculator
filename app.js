c040eaa8da9a78b7b497382f48ec8473396c9a2e
faith <faithadetona@gmail.com>
2022-09-30 11:06:54 +0100

    ''

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const keys = Array.from( document.querySelectorAll( '.function span'));
 var input = document.querySelectorAll( 'input');
-const numberkeys = keys.slice(3 , 16);
+const numberkeys = keys.slice(2 , 16);
+let x = '*' 
   numberkeys.forEach( numberkeys => {
       numberkeys.addEventListener ( 'click' , function (e) {
    var value = input[0].value += e.target.dataset.num;
@@ -9,4 +10,15 @@ const numberkeys = keys.slice(3 , 16);
 
   keys[18].onclick = () => {
     input[1].value = eval(input[0].value)
+  }
+  keys[18].addEventListener ('dblclick' , () => {
+    console.log('oka')
+  })
+
+  keys[0].onclick = () => {
+    input[0].value = input[0].value.substr(0, input[0].value.length -1)
+  } 
+  keys[1].onclick = () => {
+    input[0].value = '';
+    input[1].value = '';
   }
