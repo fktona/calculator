3dadf6df1aa63f5656867cb6d617e8002c444f88
faith <faithadetona@gmail.com>
2022-10-01 17:51:52 +0100

    okay

--- app.js
+++ app.js
@@ -1,18 +1,39 @@
-const keys = Array.from( document.querySelectorAll( '.function span'));
+
+var keys = Array.from( document.querySelectorAll( '.function span'));
 var input = document.querySelectorAll( 'input');
 const numberkeys = keys.slice(2 , 16);
-let x = '*' 
-  numberkeys.forEach( numberkeys => {
+
+
+  numberkeys.forEach( numberkeys => { 
       numberkeys.addEventListener ( 'click' , function (e) {
-   input[0].value += e.target.dataset.num,
-   input[3].style.color = 'red'
+   var value = input[0].value += e.target.dataset.num;
+   
   }
   )}) ;
-console.log(6+9)
+
+    keys.forEach(keys => {
+      keys.addEventListener ('click' , () => { 
+          keys.style.boxShadow = '2px 2px 10px #ac29ac80 ' ,
+          '-3px -3px 10px #ac29ac80' , '-3px -3px 10px #ac29ac80 ' 
+         , '2px 2px 10px #ac29ac80';
+         keys.style.transform = 'scale(0.85)'
+         
+setTimeout(() => {
+
+  keys.style.boxShadow = '3px 3px 10px #e0e0e0 ' ,
+  '-3px -3px 10px #fff' , '-3px -3px 10px #e0e0e0 ' 
+ , '3px 3px 10px #fff';
+ keys.style.transform = 'scale(1)'
+  
+}, 500);
+        
+      } ) 
+    });
 
   
 
-  keys[18].onclick = (cal) => {
+
+  keys[18].onclick = () => {
     if( input[0].value ===  '') {
       input[1].value = '';
     }
@@ -20,9 +41,6 @@ console.log(6+9)
       input[1].value = eval(input[0].value);
     }
   }
-  window.addEventListener ('dblclick' , () => {
-    console.log('oka')
-  })
 
   keys[0].onclick = () => {
     input[0].value = input[0].value.substr(0, input[0].value.length -1)
@@ -30,4 +48,5 @@ console.log(6+9)
   keys[1].onclick = () => {
     input[0].value = '';
     input[1].value = '';
+    console.log ('jui')
   }
